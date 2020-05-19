597c41616602ec7678d1a73d5c94bde873bf2725
Saltfishy <949799867@qq.com>
2020-05-19 16:43:20 +0800

    添加项目文件。

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace Supermaket
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbDiamondCard = new System.Windows.Forms.RadioButton();
+            this.rbPlatinumCard = new System.Windows.Forms.RadioButton();
+            this.rbMetalsCard = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboSates = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dgvShow = new System.Windows.Forms.DataGridView();
+            this.cmsMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).BeginInit();
+            this.cmsMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("隶书", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(149, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "超市会员管理系统";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "账号：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "密码：";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(70, 71);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(128, 21);
+            this.txtUser.TabIndex = 3;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(239, 71);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(128, 21);
+            this.txtPwd.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbDiamondCard);
+            this.groupBox1.Controls.Add(this.rbPlatinumCard);
+            this.groupBox1.Controls.Add(this.rbMetalsCard);
+            this.groupBox1.Location = new System.Drawing.Point(37, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 53);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "卡别：";
+            // 
+            // rbDiamondCard
+            // 
+            this.rbDiamondCard.AutoSize = true;
+            this.rbDiamondCard.Location = new System.Drawing.Point(247, 21);
+            this.rbDiamondCard.Name = "rbDiamondCard";
+            this.rbDiamondCard.Size = new System.Drawing.Size(59, 16);
+            this.rbDiamondCard.TabIndex = 2;
+            this.rbDiamondCard.TabStop = true;
+            this.rbDiamondCard.Text = "钻石卡";
+            this.rbDiamondCard.UseVisualStyleBackColor = true;
+            this.rbDiamondCard.Click += new System.EventHandler(this.rbDiamondCard_Click);
+            // 
+            // rbPlatinumCard
+            // 
+            this.rbPlatinumCard.AutoSize = true;
+            this.rbPlatinumCard.Location = new System.Drawing.Point(136, 21);
+            this.rbPlatinumCard.Name = "rbPlatinumCard";
+            this.rbPlatinumCard.Size = new System.Drawing.Size(59, 16);
+            this.rbPlatinumCard.TabIndex = 1;
+            this.rbPlatinumCard.TabStop = true;
+            this.rbPlatinumCard.Text = "铂金卡";
+            this.rbPlatinumCard.UseVisualStyleBackColor = true;
+            this.rbPlatinumCard.Click += new System.EventHandler(this.rbPlatinumCard_Click);
+            // 
+            // rbMetalsCard
+            // 
+            this.rbMetalsCard.AutoSize = true;
+            this.rbMetalsCard.Location = new System.Drawing.Point(32, 21);
+            this.rbMetalsCard.Name = "rbMetalsCard";
+            this.rbMetalsCard.Size = new System.Drawing.Size(47, 16);
+            this.rbMetalsCard.TabIndex = 0;
+            this.rbMetalsCard.TabStop = true;
+            this.rbMetalsCard.Text = "金卡";
+            this.rbMetalsCard.UseVisualStyleBackColor = true;
+            this.rbMetalsCard.Click += new System.EventHandler(this.rbMetalsCard_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "状态：";
+            // 
+            // cboSates
+            // 
+            this.cboSates.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSates.FormattingEnabled = true;
+            this.cboSates.Location = new System.Drawing.Point(70, 164);
+            this.cboSates.Name = "cboSates";
+            this.cboSates.Size = new System.Drawing.Size(128, 20);
+            this.cboSates.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(204, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "积分：";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(239, 163);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.ReadOnly = true;
+            this.txtScore.Size = new System.Drawing.Size(128, 21);
+            this.txtScore.TabIndex = 9;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(402, 74);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 30);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "添 加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(402, 122);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 30);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "退 出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dgvShow
+            // 
+            this.dgvShow.AllowUserToAddRows = false;
+            this.dgvShow.AllowUserToDeleteRows = false;
+            this.dgvShow.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShow.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column5,
+            this.Column1,
+            this.Column6,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvShow.ContextMenuStrip = this.cmsMenu;
+            this.dgvShow.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvShow.Location = new System.Drawing.Point(0, 203);
+            this.dgvShow.MultiSelect = false;
+            this.dgvShow.Name = "dgvShow";
+            this.dgvShow.ReadOnly = true;
+            this.dgvShow.RowTemplate.Height = 23;
+            this.dgvShow.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvShow.Size = new System.Drawing.Size(509, 247);
+            this.dgvShow.TabIndex = 12;
+            this.dgvShow.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvShow_CellClick);
+            // 
+            // cmsMenu
+            // 
+            this.cmsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiDelete});
+            this.cmsMenu.Name = "cmsMenu";
+            this.cmsMenu.Size = new System.Drawing.Size(101, 26);
+            // 
+            // tsmiDelete
+            // 
+            this.tsmiDelete.Name = "tsmiDelete";
+            this.tsmiDelete.Size = new System.Drawing.Size(100, 22);
+            this.tsmiDelete.Text = "删除";
+            this.tsmiDelete.Click += new System.EventHandler(this.tsmiDelete_Click);
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "Id";
+            this.Column5.HeaderText = "id";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "CustomerId";
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("隶书", 12F);
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "账号";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "CustomerPassword";
+            this.Column6.HeaderText = "password";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Visible = false;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "CustomerType";
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("隶书", 12F);
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "卡别";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "Score";
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("隶书", 12F);
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column3.HeaderText = "积分";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "StatesName";
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("隶书", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Column4.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Column4.HeaderText = "状态";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 450);
+            this.Controls.Add(this.dgvShow);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cboSates);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "超市会员管理系统";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).EndInit();
+            this.cmsMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbDiamondCard;
+        private System.Windows.Forms.RadioButton rbPlatinumCard;
+        private System.Windows.Forms.RadioButton rbMetalsCard;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboSates;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataGridView dgvShow;
+        private System.Windows.Forms.ContextMenuStrip cmsMenu;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDelete;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Supermaket
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        string constr = "Data Source=.;Initial Catalog=SuperMark;User ID=admin;Password=lxh6823313";
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            rbMetalsCard.Checked = true;
+            if (rbMetalsCard.Checked==true)
+            {
+                txtScore.Text = 500.ToString();
+            }
+            BindCbo(cboSates);
+            BindDgv(dgvShow);
+        }
+
+        private void BindDgv(DataGridView dgvShow)
+        {
+            DataSet ds = new DataSet();
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter adapter = new SqlDataAdapter("select u.Id,u.CustomerId,u.CustomerPassword,u.CustomerType,u.Score,s.StatesName from UsersInfo as u ,States as s where s.Id=u.StatesId", con);
+            adapter.Fill(ds,"show");
+            dgvShow.DataSource = ds.Tables[0];
+        }
+
+        private void rbMetalsCard_Click(object sender, EventArgs e)
+        {
+            txtScore.Text = 500.ToString();
+        }
+
+        private void rbPlatinumCard_Click(object sender, EventArgs e)
+        {
+            txtScore.Text = 2000.ToString();
+        }
+
+        private void rbDiamondCard_Click(object sender, EventArgs e)
+        {
+            txtScore.Text = 5000.ToString();
+        }
+
+
+        public bool chekinput(TextBox box)
+        {
+            if (!string.IsNullOrEmpty(box.Text.Trim()))
+            {
+                
+                return false;
+            }
+            box.Focus();
+            return true;
+        }
+
+        public void BindCbo(ComboBox box)
+        {
+            DataSet ds = new DataSet();
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from dbo.States",con);
+            adapter.Fill(ds,"States");
+            box.DataSource = ds.Tables[0];
+            box.DisplayMember = "StatesName";
+            box.ValueMember = "Id";
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string type = null;
+            if (rbDiamondCard.Checked==true)
+            {
+                type = rbDiamondCard.Text;
+            }
+            if (rbMetalsCard.Checked==true)
+            {
+                type = rbMetalsCard.Text;
+            }
+            if (rbPlatinumCard.Checked==true)
+            {
+                type = rbPlatinumCard.Text;
+            }
+            if (chekinput(txtUser) || chekinput(txtPwd))
+            {
+                MessageBox.Show("账户密码都不能为空！");
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection(constr);
+                con.Open();
+                SqlCommand cmd = new SqlCommand(string.Format("insert into UsersInfo(CustomerId,CustomerPassword,CustomerType,Score,StatesId) Values('{0}','{1}','{2}','{3}','{4}')",txtUser.Text.Trim(),txtPwd.Text.Trim(),type,txtScore.Text,cboSates.SelectedValue), con);
+                int isok=cmd.ExecuteNonQuery();
+                if (isok>0)
+                {
+                    MessageBox.Show("添加成功！");
+                    FrmMain_Load(sender,e);
+                }
+            }
+        }
+
+        private void dgvShow_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvShow.SelectedRows[0].Cells[3].Value.ToString()==rbMetalsCard.Text)
+            {
+                rbMetalsCard.Checked = true;
+            }else if (dgvShow.SelectedRows[0].Cells[3].Value.ToString() == rbPlatinumCard.Text)
+            {
+                rbPlatinumCard.Checked = true;
+            }
+            else if (dgvShow.SelectedRows[0].Cells[3].Value.ToString() == rbDiamondCard.Text)
+            {
+                rbDiamondCard.Checked = true;
+            }
+            txtPwd.Text = dgvShow.SelectedRows[0].Cells[2].Value.ToString();
+            txtUser.Text= dgvShow.SelectedRows[0].Cells[1].Value.ToString();
+            txtScore.Text = dgvShow.SelectedRows[0].Cells[4].Value.ToString();
+            cboSates.Text = dgvShow.SelectedRows[0].Cells[5].Value.ToString();
+        }
+
+        private void tsmiDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(string.Format("delete UsersInfo where Id={0}",dgvShow.SelectedRows[0].Cells[0].Value), con);
+            int isok = cmd.ExecuteNonQuery();
+            if (isok > 0)
+            {
+                MessageBox.Show("删除成功！");
+                FrmMain_Load(sender, e);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
